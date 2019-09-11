d004bec2cd284dbf4afdaf96fc1b1bc9eaa7c96a
rixo <rixo@rixo.fr>
2019-09-11 02:33:43 +0200

    expose new utils

--- core.js
+++
@@ -1,3 +0,0 @@
-export { createConnector } from './lib/connect'
-
-export { binder } from './lib/binder'
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { getContext, setContext, onDestroy } from 'svelte'
 
-import { createConnector } from './core'
+import { createConnector } from './lib/connect'
 
-export { binder } from './core'
+export { binder } from './lib/binder'
+
+export { default as connectable } from './lib/connectable'
 
 export const connect = createConnector({
   getContext,
